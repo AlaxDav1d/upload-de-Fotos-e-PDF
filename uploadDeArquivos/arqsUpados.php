b48eff210340344bf1c29bdb42219c9e9939b379
Alax David Oliveira <127566072+AlaxDav1d@users.noreply.github.com>
2024-05-09 21:19:34 -0300

    upload da versão mais recente
    Nesta versão eu adicionei o CSS na pagina Inicial e na Pagina de visualização, depois vou adicionar estilo na pagina de visualização de PDF;
    Corrigi o erro de envio de arquivo quando a pagina é recarregada;
    Pro futuro irei adicionar funcionalidades de exclusão de arquivo e edição de nome para o usuario que realizou o upload

--- uploadDeArquivos/arqsUpados.php
+++ uploadDeArquivos/arqsUpados.php
@@ -3,27 +3,131 @@
      $newQuery = "SELECT * FROM imagens WHERE caminho LIKE '%.jpg%' OR caminho LIKE '%.png%' OR caminho LIKE '%.gif%'";
      $sql = $mysqli->query($newQuery) or die($mysqli->error);
 
-?>
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html lang="en">
 <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>Arquivos Salvos</title>
+     <style>
+          *{
+               margin: 0;
+               padding: 0;
+               box-sizing: border-box;
+          }
+          body{
+               display: flex;
+               width: 100%;
+               height: 100%;
+               flex-direction: column;
+               align-items: center;
+               justify-content: center;
+               background-color: rgb(40, 92, 92);
+          }
+          .top{
+               display: flex;
+               width: 100%;
+               justify-content: flex-start;
+               align-items: center;
+               font-family: 'Trebuchet MS', sans-serif;
+          }
+          .top img{
+               width: 100%;
+               margin-left: 3.5%;
+               height: 100%;
+          }
+          .top h1{
+               text-align: center;
+               margin: 2.5%;
+               margin-left: 23%;
+               color: #fff;
+          }
+          .top a{
+               margin-left: 3.5%;
+               width: 5%;
+               height: 5%;
+          }
+          .fotos{
+               display: grid;
+               grid-template-columns: repeat(3, 1fr); /* Define 3 colunas com largura igual */
+               grid-gap: 30px;
+               row-gap: 30px;
+               gap: 2%;
+               margin-bottom: 5%;
+               align-items: center;
+               justify-content: center;
+          }    
+          .foto{
+               display: flex;
+               flex-direction: column;
+               align-items: center;
+               justify-content: center;
+               padding: 2%;
+               gap: 10%;
+               height: 350px;
+               width: 450px;
+               background-color: transparent;
+               color: #fff;
+               font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+               font-size: 3vh;
+               text-transform: capitalize;
+               border: 5px black double;
+               text-shadow: 0.1px 0.1px 10px white;
+               border-radius: 5px;
+               transition: 300ms;
+          }
+          .foto img:hover{
+               box-shadow: -0.5px -0.9px 10px white;
+          }
+          .foto img{
+               border: 1px solid black;
+               width: 90%; 
+               height: 70%;
+               transition: 300ms;
+               cursor: pointer;
+               border-radius: 5px;
+          }
+          #link{
+               user-select: none;
+               color: #fff;
+               width: 40%;
+               height: 30%;
+               margin: 2%;
+               font-size: 2.5vh;
+               border: 1px solid black;
+               padding: 1%;
+               text-align: center;
+               border-radius: 5px;
+               transition: 400ms;
+               text-transform: capitalize;
+               text-shadow: 0.1px 1px 10px black;
+               text-decoration: none;
+          }
+          #link:hover{
+               background-color: #000;
+               text-shadow: 0.1px 0.1px 10px white;
+          }
+     </style>
 </head>
-<style>
-     .foto{
-          max-width: 200px;
-          max-height: 180px;
-     }
-</style>
 <body>
-     <h1>Aqui voce vera os arquivos que estao salvos</h1>
+     <div class="top">
+          <a href="index.php" title="Voltar a Página Inicial" ><img src="backWard.png" alt=""></a>
+          <h1>Aqui você verá os arquivos que estão salvos</h1>
+     </div>
+     <div class="fotos">
      <?php 
           while($linhas = $sql->fetch_assoc()){
-               echo "<img src='$linhas[caminho]' class='foto'>";
+               echo" 
+               <div class='foto'>
+               <h3>$linhas[nomeReal]</h3>
+               <img src='$linhas[caminho]'>
+               </div>
+               ";
           }
      ?>
-    <a href="index.php">Clique aqui para voltar para a tela Inicial</a>
+     </div>
+
+    <a href="index.php" id="link">Clique aqui para voltar para a Pagina inicial</a>
+    
 </body>
 </html>
--- uploadDeArquivos/index.php
+++ uploadDeArquivos/index.php
@@ -1,12 +1,21 @@
 <?php
-     include('conexao.php');
-     $arquivo = $_FILES['arquivo'];
-     $localArmazenamento = 'arquivosUp/';
-     $nameFile = $arquivo['name'];
-     $newName = uniqid();
-     $extFile = strtolower(pathinfo($arquivo['name'],PATHINFO_EXTENSION));
-     $caminho = $localArmazenamento . $newName . '.' . $extFile;
-     $sqlQuery = "INSERT INTO imagens(nomeArq,caminho) VALUES('$newName','$caminho')";
+include('conexao.php');
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['arquivo'])) {
+    $arquivo = $_FILES['arquivo'];
+    $localArmazenamento = 'arquivosUp/';
+    $newName = uniqid();
+    $extFile = strtolower(pathinfo($arquivo['name'], PATHINFO_EXTENSION));
+
+    if ($extFile != 'png' && $extFile != 'jpg' && $extFile != 'pdf' && $extFile != 'gif') {
+        echo '<script>alert("Permitido somente arquivos: JPG, PNG, GIF ou PDF")</script>';
+    } else {
+        $caminho = $localArmazenamento . $newName . '.' . $extFile;
+        move_uploaded_file($arquivo['tmp_name'], $caminho);
+        $sqlQuery = "INSERT INTO imagens(nomeArq, caminho, nomeReal) VALUES ('$newName', '$caminho', '{$arquivo['name']}')";
+        $mysqli->query($sqlQuery) or die($mysqli->error);
+        echo '<script>alert("Arquivo enviado com sucesso!!")</script>';
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -17,38 +26,107 @@
      <title>Upload de Arquivos</title>
 </head>
 <style>
+          
      body{
-          color: white;
+          font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+          margin: 0;
      }
-     
      .all{
           display: flex;
           align-items: center;
-          justify-content: center;
+          justify-content: flex-start;
           flex-direction: column;
           font-size: 4vh;
-          color: black;
+          background-color: rgb(40, 92, 92);
+          height: 100vh;
+          color: #fff;
      }
+     .links{
+          display: flex;
+          align-self: center;
+          margin-left: -25%;
+          justify-content: space-around;
+          width: 150%;
+     }
+     h1{
+          margin: 5% 0 7%;
+     }
+     form{
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          width: 100%;
+          margin-bottom: 15%;
+     }
+     input[type='file']{
+          display: none;
+     }
+     label{
+          background-color: black;
+          padding: 5%;
+          margin: 2%;
+          z-index: 1;
+          cursor: pointer;
+          border-radius: 5px;
+          transition: 200ms;
+          user-select: none;
+     }
+     label:hover{
+          box-shadow: -0.1px 1px 15px black;
+
+     }
+     label:active{
+          transform: scale(0.990);
+     }
+     a{
+          user-select: none;
+          color: #fff;
+          width: 90%;
+          margin: 2%;
+          border: 1px solid black;
+          padding: 2.5% 5%;
+          border-radius: 5px;
+          transition: 400ms;
+          text-transform: capitalize;
+          text-shadow: 0.1px 1px 10px black;
+          text-decoration: none;
+     }
+     a:hover{
+          background-color: #000;
+          text-shadow: none;
+     }
+     input[type='submit']{
+          margin-top: 3%;
+          background-color: transparent;
+          color: #fff;
+          height: 4vh;
+          width: 25vh;
+          border: 1px solid black;
+          cursor: pointer;
+          transition: 200ms;
+          border-radius: 5px;
+          font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+     }
+     input[type='submit']:hover{
+          background-color:rgb(37, 84, 84);
+     }
+
 </style>
 <body>    
      <div class="all">
-     <h1>Upload de Arquivos</h1>
-     <form action="" method="POST" enctype="multipart/form-data">
-          <input type="file" name="arquivo">
-          <input type="submit" value="Enviar Arquivo">
-          
-          <?php
-                    if($extFile != 'png' && $extFile != 'jpg' && $extFile != 'pdf' && $extFile != 'gif'){
-                         echo '<script>alert("Permitido somente arquivos: JPG,PNG,GIF ou PDF")</script>';
-                    }else{
-                         move_uploaded_file($arquivo['tmp_name'],$localArmazenamento . $newName . "." . $extFile);
-                         $mysqli->query($sqlQuery) or die($mysqli->error);
-                         echo '<script>alert("Arquivo enviado com sucesso!!")</script>';
-                    }
-          ?>
-     </form>
-     <a href="arqsUpados.php">Veja as Fotos aqui</a>
-     <a href="pdfs.php">Veja os PDF's aqui</a>
+          <h1>Upload de Arquivos</h1>
+          <div class="tudo">
+               <form action="" method="POST" enctype="multipart/form-data">
+                    <label for="file">Selecionar Arquivo</label>
+                    <input type="file" name="arquivo" id="file">
+                    <input type="submit" value="Enviar">
+               </form>
+               <div class="links">
+                    <a href="arqsUpados.php">Veja as Fotos aqui</a>
+                    <a href="pdfs.php">Veja os PDF's aqui</a>
+               </div>
+          </div>
      </div>
 </body>
 </html>
